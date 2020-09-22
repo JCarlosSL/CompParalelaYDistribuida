1819550cb833f254827cd62f06c82113c8c59f97
Jean Carlos soncco lupa <Jean Carlos soncco lupa>
2020-09-21 23:22:25 -0500

    update ejercicio 3

--- Practica1/Ejercicio_3.cpp
+++ Practica1/Ejercicio_3.cpp
@@ -3,21 +3,46 @@
 
 using namespace std;
     
-void executeMByBloques(Matrix A, Matrix B,Matrix C,int bsize){
+void executeMNoBloques(Matrix A, Matrix B,Matrix C){
     auto t1 = std::chrono::high_resolution_clock::now();
-    A.multiplicacionBloques6(&B,&C,bsize);
+    A.multiplicacion(&B,&C);
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
-    cout<<"tiempo: "<<duration/1000.0<<"\n";
+    cout<<"Sin Bloques-tiempo: "<<duration/1000.0<<"\n";
+}
+
+void executeMByBloques(Matrix A, Matrix B,Matrix C,int bsize){
+    auto t1 = std::chrono::high_resolution_clock::now();
+    A.multiplicacionBloques(&B,&C,bsize);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
+    cout<<"Con Bloques-tiempo: "<<duration/1000.0<<"\n";
 }
 
 int main() {
     int n=100;
     int bsize = 10;
-    cout<<"-- Multiplicación de Matrices cuadraticas por bloques-- \n";
+    /*cout<<"--Test de Multiplicación de Matrices cuadraticas por bloques-- \n";
+    cout<<"bloques de "<<bsize<<endl;
     for(int i=0 ; i<20 ; ++i){
+        
+        cout<<"Test "<<i+1<<" con n="<<n<<"\n";
         Matrix A(n),B(n),C(n);
-        executeMByBloques(A,B,C,bsize);      
+        executeMByBloques(A,B,C,bsize);   
+        A.freeMatrix();
+        B.freeMatrix();
+        C.freeMatrix();
+        n += 100;
+    }*/
+
+    cout<<"--Test Multiplicacion de matrices -- \n";
+    cout<<"--Matrices sin bloque vs Matrices por bloques-- \n";
+    cout<<"--Bloques de 10-- \n";
+    for(int i=0; i<20 ; ++i){
+        cout<<"Test "<<i+1<<" con n="<<n<<"\n";
+        Matrix A(n),B(n),C(n);
+        executeMNoBloques(A,B,C);
+        executeMByBloques(A,B,C,bsize);
         A.freeMatrix();
         B.freeMatrix();
         C.freeMatrix();
